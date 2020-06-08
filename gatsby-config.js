8541f01e0869ba9aa4b47b59c2bc374bcc610128
BuildTools <and.richardson@live.co.uk>
2020-06-08 21:06:18 +0100

    changed filename to assets for images in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/static/assets`,
-        name: `images`,
+        name: `assets`,
       },
     },
     {
