41debd71b83f89b816fa1b847c2d19bdab572e36
BuildTools <and.richardson@live.co.uk>
2020-08-02 16:31:26 +0100

    added manifest file and favicon - needs more clarity

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,29 @@
 module.exports = {
   siteMetadata: {
-    title: `Trust this Feast`,
+    title: `Trust This Feast`,
     description: `Cool food here`,
     author: `@andrewsrichardson`,
   },
 
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Trust This Feast",
+        short_name: "Trust This Feast",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/logo.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
