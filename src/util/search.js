23b2db7602062af9180f296c73973c372dd1820e
BuildTools <and.richardson@live.co.uk>
2020-08-25 16:14:16 +0100

    typecheck on window to stop build error

--- src/util/search.js
+++ src/util/search.js
@@ -1,20 +1,23 @@
 export default function search(query) {
-  var index = window.__FLEXSEARCH__.en.index
-  var store = window.__FLEXSEARCH__.en.store
-  if (!query || !index) {
-    return []
-  } else {
-    var results = []
-    Object.keys(index).forEach(idx => {
-      results.push(...index[idx].values.search(query))
-    })
+  if (typeof window !== undefined) {
+    var index = window.__FLEXSEARCH__.en.index
+    var store = window.__FLEXSEARCH__.en.store
+    if (!query || !index) {
+      return []
+    } else {
+      var results = []
+      Object.keys(index).forEach(idx => {
+        results.push(...index[idx].values.search(query))
+      })
 
-    results = Array.from(new Set(results))
+      results = Array.from(new Set(results))
 
-    var nodes = store
-      .filter(node => (results.includes(node.id) ? node : null))
-      .map(node => node.node)
+      var nodes = store
+        .filter(node => (results.includes(node.id) ? node : null))
+        .map(node => node.node)
 
-    return nodes
+      return nodes
+    }
   }
+  return null
 }
