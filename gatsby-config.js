00332ed55afe9c848e62ea9138053a34391309e5
BuildTools <and.richardson@live.co.uk>
2020-09-01 18:31:04 +0100

    changed ga tracking to bare min

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,27 +15,6 @@ module.exports = {
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GA_TRACKING_ID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: [],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "",
-        // Enables Google Optimize Experiment ID
-        experimentId: "",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "0",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
       },
     },
     {
