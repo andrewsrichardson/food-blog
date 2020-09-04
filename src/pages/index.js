f3153515f5b4daf86526cdd9b7fd64dc5b936529
BuildTools <and.richardson@live.co.uk>
2020-09-04 20:21:40 +0100

    added description section with button to recipes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,28 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PostList from "../components/PostList/PostList"
 import Carousel from "../components/Carousel/Carousel"
+import "./index.css"
+import { navigate } from "gatsby"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Carousel />
+    <section className="description">
+      <h2>
+        🍜Hearty hella-seasoned food 🌮Lazy munches, tasty brunches, simple
+        lunches and big fat feasts ♥️ Uncomplicated and so worth it
+        #TrustThisFeast
+      </h2>
+      <button
+        className="bubbly-button"
+        onClick={() => {
+          navigate("/categories")
+        }}
+      >
+        All Recipes
+      </button>
+    </section>
     <PostList searchFilter={""} />
   </Layout>
 )
