e39a96768bd68520d4fbfbacc3fb4e62e06c2079
BuildTools <and.richardson@live.co.uk>
2020-10-24 18:26:56 +0100

    replaced divs with img for header nav icon

--- src/components/Header/ToggleMenuButton.js
+++ src/components/Header/ToggleMenuButton.js
@@ -1,16 +1,33 @@
-import React from "react";
-import './ToggleMenuButton.css'
+import GatsbyImage from "gatsby-image"
+import React from "react"
+import "./ToggleMenuButton.css"
+import { graphql, useStaticQuery } from "gatsby"
 
+function ToggleMenuButton(props) {
+  const data = useStaticQuery(graphql`
+    query MenuQuery {
+      menu: allFile(filter: { relativePath: { eq: "ellipsis.png" } }) {
+        nodes {
+          childImageSharp {
+            fixed(width: 37, height: 9) {
+              aspectRatio
+              base64
+              height
+              src
+              srcSet
+              width
+            }
+          }
+        }
+      }
+    }
+  `)
 
-
-const ToggleMenuButton = props => (
-    
+  return (
     <button className="toggle-button" onClick={props.click}>
-      <div className= "toggle-button_line"></div>
-      <div className= "toggle-button_line"></div>
-      <div className= "toggle-button_line"></div>
+      <GatsbyImage fixed={data.menu.nodes[0].childImageSharp.fixed} />
     </button>
-    
-);
+  )
+}
 
 export default ToggleMenuButton
